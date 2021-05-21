6c0d10c96939a656ced299f84019e26d33ee7827
casey <406326-casey@users.noreply.gitlab.com>
2021-05-20 21:04:29 -0400

    Cleanup main class for first test release

--- src/main/java/easel/Easel.java
+++ src/main/java/easel/Easel.java
@@ -2,23 +2,13 @@ package easel;
 
 import basemod.BaseMod;
 import basemod.interfaces.PostInitializeSubscriber;
-import basemod.interfaces.PostUpdateSubscriber;
-import basemod.interfaces.RenderSubscriber;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
-import com.megacrit.cardcrawl.core.Settings;
-import com.megacrit.cardcrawl.helpers.ImageMaster;
-import easel.ui.AbstractWidget;
-import easel.ui.debug.NinePatchTest;
 import easel.utils.textures.TextureManager;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ArrayList;
-
 @SpireInitializer
-public class Easel implements PostInitializeSubscriber, RenderSubscriber, PostUpdateSubscriber {
+public class Easel implements PostInitializeSubscriber {
     public static final Logger logger = LogManager.getLogger(Easel.class);
 
     public static void initialize() {
@@ -29,94 +19,8 @@ public class Easel implements PostInitializeSubscriber, RenderSubscriber, PostUp
         BaseMod.subscribe(this);
     }
 
-    private ArrayList<AbstractWidget> widgets = new ArrayList<>();
-
-    @Override
-    public void receivePostUpdate() {
-        widgets.forEach(AbstractWidget::update);
-    }
-
     @Override
     public void receivePostInitialize() {
-        logger.info("Hello, world");
         TextureManager.loadTextures();
-
-        widgets.add(
-                new NinePatchTest()
-                        .makeMoveable()
-//                        .onMouseEnter(w -> SoundHelper.uiHoverSound())
-//                        .onLeftClick(w -> SoundHelper.uiClick1())
-//                        .onRightClick(w -> SoundHelper.uiClick2())
-                        .anchoredCenteredOnScreen()
-        );
-
-
-//        GridLayout grid = new GridLayout()
-//                .withRowsCols(500, 500, 3, 3)
-//                .withChildrenInRow(0, new DebugWidget(), new DebugWidget(), new DebugWidget())
-//                .withChildrenInRow(1, new DebugWidget(), new DebugWidget(), new DebugWidget())
-//                .withChildrenInRow(2, new DebugWidget(), new DebugWidget(), new DebugWidget())
-//                .withChild(1, 1, background, AnchorPosition.CENTER)
-//                //.withBackgroundWidget(background.scaleToFullWidget(grid))
-//                .anchoredCenteredOnScreen();
-
-
-//        GridLayout grid = new GridLayout();
-//        widgets.add(
-//                grid.withRowsCols(500, 500, 2, 2)
-//                        .withDefaultChildAnchorPosition(AnchorPosition.CENTER)
-//                        .withChildrenInRow(0,
-//                                new DebugWidget()
-//                                        .onMouseEnter(w -> w.setColor(DebugWidget.DEBUG_COLOR_1))
-//                                        .onMouseLeave(w -> w.setColor(DebugWidget.DEBUG_COLOR_0))
-//                                        .onLeftClick(w -> SoundHelper.cawCaw()),
-//                                new DebugWidget()
-//                                        .onMouseEnter(w -> w.setColor(DebugWidget.DEBUG_COLOR_1))
-//                                        .onMouseLeave(w -> w.setColor(DebugWidget.DEBUG_COLOR_0))
-//                                        .onLeftClick(w -> SoundHelper.cawCaw())
-//                        )
-//                        .withChildrenInRow(1,
-//                                new DebugWidget()
-//                                        .onMouseEnter(w -> w.setColor(DebugWidget.DEBUG_COLOR_1))
-//                                        .onMouseLeave(w -> w.setColor(DebugWidget.DEBUG_COLOR_0))
-//                                        .onRightClick(w -> SoundHelper.cawCaw()),
-//                                new MoveableWidget(100, 100, grid)
-//                        )
-//                        //.resizeRowsToFitTallestChildren()
-//                        .onLeftClick(onClick -> SoundHelper.screenCloseSound())
-//                        .anchoredCenteredOnScreen()
-//        );
-//
-//        widgets.add(
-//                new DebugWidget(100, 100, DebugWidget.DEBUG_COLOR_1)
-//                        .onRightClick(w -> {
-//                            w.setColor(Color.GOLD);
-//                            SoundHelper.cawCaw();
-//                        })
-//                        .onMouseEnter(w -> w.setColor(DebugWidget.DEBUG_COLOR_0))
-//                        .onMouseLeave(w -> w.setColor(DebugWidget.DEBUG_COLOR_1))
-//                        .makeMoveable()
-//                        .anchoredAt(500, 500, AnchorPosition.LEFT_BOTTOM)
-////                new InteractiveDebugWidget()
-////                        .onLeftClick(w -> SoundHelper.cawCaw())
-////                        .onRightClick(w -> w.setColor(Color.GOLD))
-////                        .anchoredAt(500, 500, AnchorPosition.LEFT_BOTTOM)
-////                new PartiallyMoveableWidget(200, 140)
-////                        .anchoredCenteredOnScreen()
-//        );
-//
-//        widgets.add(
-//                new DebugWidget(50, 50)
-//                        .anchoredAt(100, 100, AnchorPosition.LEFT_BOTTOM)
-//        );
-    }
-
-
-    @Override
-    public void receiveRender(SpriteBatch sb) {
-        sb.setColor(Color.BLACK);
-        sb.draw(ImageMaster.WHITE_SQUARE_IMG, 0, 0, Settings.WIDTH, Settings.HEIGHT);
-
-        widgets.forEach(w -> w.render(sb));
     }
 }
