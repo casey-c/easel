b1c45bb8372b8e6ba4d9e1298f83ee7419c44d07
casey <406326-casey@users.noreply.gitlab.com>
2021-05-26 21:28:24 -0400

    Remove env. vars and mod tasks

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,53 +14,53 @@ Setup environment variables
     - NOTE: these compileOnlyLibs are not included in the JAR, so you will get runtime
       errors if you try and call code that won't exist on the client.
  */
-var stsInstallLocation: String = System.getenv("STS_INSTALL")
-var compileOnlyLibs: String = System.getenv("STS_MODDING_LIB")
+//var stsInstallLocation: String = System.getenv("STS_INSTALL")
+//var compileOnlyLibs: String = System.getenv("STS_MODDING_LIB")
 
 // Uses the value written in settings.gradle
 var modName: String = rootProject.name
 
-dependencies {
-    compileOnly(fileTree(compileOnlyLibs))
-}
+//dependencies {
+//    compileOnly(fileTree(compileOnlyLibs))
+//}
 
 // --------------------------------------------------------------------------------
 
-tasks.register<Jar>("buildJavadocJAR") {
-    group = "Slay the Spire"
-    description = "Builds the javadoc jar"
-
-    dependsOn("javadoc")
-    archiveClassifier.set("javadoc")
-}
-
-tasks.register<Jar>("buildJAR") {
-    group = "Slay the Spire"
-    description = "Builds a fat (includes runtime dependencies) JAR in the build/libs folder"
-
-    // Main code
-    from(sourceSets.main.get().output)
-
-    // Any runtime dependencies (e.g. from mavenCentral(), local JARs, etc.)
-    dependsOn(configurations.runtimeClasspath)
-    from({
-        configurations.runtimeClasspath.get().filter {
-            it.name.endsWith("jar")
-        }.map {
-            zipTree(it)
-        }
-    })
-}
-
-tasks.register<Copy>("buildAndCopyJAR") {
-    group = "Slay the Spire"
-    description = "Copies the JAR from your build/libs folder into your \$STS_INSTALL location"
-
-    dependsOn("buildJAR")
-
-    from("build/libs/$modName.jar")
-    into("$stsInstallLocation\\mods")
-}
+//tasks.register<Jar>("buildJavadocJAR") {
+//    group = "Slay the Spire"
+//    description = "Builds the javadoc jar"
+//
+//    dependsOn("javadoc")
+//    archiveClassifier.set("javadoc")
+//}
+//
+//tasks.register<Jar>("buildJAR") {
+//    group = "Slay the Spire"
+//    description = "Builds a fat (includes runtime dependencies) JAR in the build/libs folder"
+//
+//    // Main code
+//    from(sourceSets.main.get().output)
+//
+//    // Any runtime dependencies (e.g. from mavenCentral(), local JARs, etc.)
+//    dependsOn(configurations.runtimeClasspath)
+//    from({
+//        configurations.runtimeClasspath.get().filter {
+//            it.name.endsWith("jar")
+//        }.map {
+//            zipTree(it)
+//        }
+//    })
+//}
+//
+//tasks.register<Copy>("buildAndCopyJAR") {
+//    group = "Slay the Spire"
+//    description = "Copies the JAR from your build/libs folder into your \$STS_INSTALL location"
+//
+//    dependsOn("buildJAR")
+//
+//    from("build/libs/$modName.jar")
+//    into("$stsInstallLocation\\mods")
+//}
 
 // --------------------------------------------------------------------------------
 
