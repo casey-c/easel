6b7a154ea6c7966deccd7d7f65e332d8e7899e59
casey <406326-casey@users.noreply.gitlab.com>
2021-06-15 17:43:45 -0400

    SmartLabels: labels with in-line color changes, newlines, and text wrapping

--- src/main/java/easel/Easel.java
+++ src/main/java/easel/Easel.java
@@ -4,8 +4,11 @@ import basemod.BaseMod;
 import basemod.interfaces.PostInitializeSubscriber;
 import basemod.interfaces.PostUpdateSubscriber;
 import basemod.interfaces.RenderSubscriber;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.evacipated.cardcrawl.modthespire.lib.SpireInitializer;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
 import easel.config.EaselConfigHelper;
 import easel.config.enums.ConfigIntegerEnum;
 import easel.config.enums.ConfigStringEnum;
@@ -15,8 +18,12 @@ import easel.ui.AnchorPosition;
 import easel.ui.containers.MoveContainer;
 import easel.ui.containers.StyledContainer;
 import easel.ui.layouts.HorizontalLayout;
+import easel.ui.layouts.VerticalLayout;
 import easel.ui.text.Label;
+import easel.ui.text.SmartLabel;
 import easel.utils.EaselFonts;
+import easel.utils.EaselSoundHelper;
+import easel.utils.colors.EaselColors;
 import easel.utils.textures.TextureLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -36,80 +43,60 @@ public class Easel implements PostInitializeSubscriber, RenderSubscriber, PostUp
     }
 
     private ArrayList<AbstractWidget> widgets = new ArrayList<>();
-//    private AnchorPosition ap = AnchorPosition.LEFT_TOP;
-//    private enum SwapWindows { MAIN, SECONDARY };
-
-    private ArrayList<StyledContainer> containers = new ArrayList<>();
-
-    private EaselConfigHelper<SampleBooleanChoices, ConfigIntegerEnum, TestStringOptions> configHelper;
-    private float delta = 80;
-
-    private enum TestStringOptions implements ConfigStringEnum {
-        MOVE_CONTAINER_LOCATIONS("");
-
-        String val;
-        TestStringOptions(String val) { this.val = val; }
-        @Override public String getDefault() { return val; }
-    }
 
     @Override
     public void receivePostInitialize() {
         TextureLoader.loadTextures();
         EaselFonts.loadFonts();
 
-//        MoveContainer moveContainer =
-//                new MoveContainer()
-//                        .withChild(new Label("First").withMargins(20))
-//                        .withChild(new Label("Second").withMargins(20))
-//                        .withChild(new Label("Third").withMargins(20))
-//                        .withChild(new Label("Fourth").withMargins(20));
-//
-//        widgets.add(
-//                moveContainer
-//        );
+        // Tests
+        VerticalLayout layout = new VerticalLayout(10, 20)
+                .withChild(
+                        new StyledContainer(100, 100)
+                                .withHeader("Tip Body Font")
+                                .withHeaderColor(EaselColors.HEADER_STRONG_PURPLE())
+                                .withContent(
+                                        new SmartLabel(FontHelper.tipBodyFont, 500, 10)
+                                                .withTextColor(Settings.CREAM_COLOR)
+                                                .withText("Hello, world.", Settings.RED_TEXT_COLOR)
+                                                .withText(" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore")
+                                                .withText(" magna aliqua.", Settings.BLUE_TEXT_COLOR)
+                                                .withText(" Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                                                .onRightClick(label -> {
+                                                    EaselSoundHelper.cawCaw();
+                                                }),
+                                        true
+                                )
+                                .scaleToContent()
+                )
+                .withChild(
+                        new StyledContainer(100, 100)
+                                .withHeader("Easel Italic Font")
+                                .withHeaderColor(EaselColors.HEADER_LIGHT_ALGAE())
+                                .withContent(
+                                        new SmartLabel(EaselFonts.MEDIUM_ITALIC, 300, 10)
+                                                .withText("Hello, world.")
+                                                .withNewlines(1)
+                                                .withText("Hello, world.")
+                                                .withNewlines(2)
+                                                .withText("Hello, world.")
+                                                .withNewlines(2)
+                                                .withText(" Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+                                                .withNewlines(2)
+                                                .withText(" Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+                                                .onRightClick(label -> {
+                                                    EaselSoundHelper.cawCaw();
+                                                }),
+                                true
+                                )
+                                .scaleToContent()
+                )
+                .anchoredCenteredOnScreen()
+                ;
 
-//        HorizontalLayout layout = new HorizontalLayout(100, 20)
-//                .withChild(new StyledContainer(300, 200).withHeader("One").withContent(new Label("ONE"), true))
-//                .withChild(new StyledContainer(300, 200).withHeader("Two").withContent(new Label("TWO"), true))
-//                .withChild(new StyledContainer(300, 200).withHeader("Three").withContent(new Label("THREE"), true))
-//                .anchoredCenteredOnScreen();
-        StyledContainer a = new StyledContainer(300, 200)
-                .withHeader("One")
-                .withContent(new Label("ONE"), true);
-        StyledContainer b = new StyledContainer(300, 200)
-                .withHeader("Two")
-                .withContent(new Label("TWO"), true);
-        StyledContainer c = new StyledContainer(300, 200)
-                .withHeader("Three")
-                .withContent(new Label("THREE"), true);
-        StyledContainer d = new StyledContainer(300, 200)
-                .withHeader("Four")
-                .withContent(new Label("FOUR"), true);
-
-        HorizontalLayout layout = new HorizontalLayout(100, -40)
-                .withDefaultChildAnchorPosition(AnchorPosition.CENTER)
-                .withChild(a)
-                .withChild(b)
-                .withChild(c)
-                .withChild(d)
-                .scaleToTallestChild()
-                .anchoredCenteredOnScreen();
-
-        configHelper = EaselConfigHelper.fromBooleansStrings("Easel", "EaselMoveTests", SampleBooleanChoices.class, TestStringOptions.class);
-
-        MoveContainer mc = new MoveContainer()
-                .withAllChildrenOfLayout(layout)
-                .onRightClick(container -> {
-                    configHelper.setString(TestStringOptions.MOVE_CONTAINER_LOCATIONS, container.toJsonString());
-                })
-                .anchoredCenteredOnScreen();
-
-        // Load defaults
-        mc.loadFromJsonString(configHelper.getString(TestStringOptions.MOVE_CONTAINER_LOCATIONS));
-
-        widgets.add(mc);
-
-        System.out.println(configHelper);
+        widgets.add(
+                new MoveContainer().withAllChildrenOfLayout(layout)
+        );
     }
 
 
--- src/main/java/easel/ui/text/SmartLabel.java
+++ src/main/java/easel/ui/text/SmartLabel.java
@@ -1,16 +1,205 @@
 package easel.ui.text;
 
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.megacrit.cardcrawl.core.Settings;
+import com.megacrit.cardcrawl.helpers.FontHelper;
 import easel.ui.AbstractWidget;
+import easel.utils.EaselGraphicsHelper;
 
-//public class SmartLabel extends AbstractWidget<SmartLabel> {
-//    //private String text;
+import java.util.ArrayList;
+
+public class SmartLabel extends AbstractWidget<SmartLabel> {
+    private float textHeight;
+    private float textWidth;
+
+    private float lineWidth;
+    private float lineSpacing;
+
+    private BitmapFont font;
+    private Color currColor = Settings.CREAM_COLOR;
+
+    private static final class TextGroup {
+        String text;
+        int line;
+        Color color;
+        float width;
+
+        public TextGroup(String text, int line, Color color, float width) {
+            this.text = text;
+            this.line = line;
+            this.color = color;
+            this.width = width;
+        }
+    }
+
+    private ArrayList<TextGroup> groups = new ArrayList<>();
+
+    private float spaceWidth;
+    private float lastLeft;
+    private int lastLine;
+
+    private float fontLineHeight;
+    private boolean startOfLine = true;
+
+    private StringBuilder stringBuilder = new StringBuilder();
+
+    // --------------------------------------------------------------------------------
+
+    public SmartLabel(BitmapFont font, float lineWidth, float lineSpacing) {
+        this.font = font;
+
+        this.spaceWidth = FontHelper.getWidth(font, " ", 1.0f) / Settings.scale;
+        this.fontLineHeight = FontHelper.getHeight(font) / Settings.scale;
+
+        this.lineWidth = lineWidth;
+        this.lineSpacing = lineSpacing;
+    }
+
+    // --------------------------------------------------------------------------------
+
+    public SmartLabel withText(String text) {
+        appendTextByWords(text);
+        return this;
+    }
+
+    public SmartLabel withText(String text, Color textColor) {
+        Color previousColor = currColor;
+        this.currColor = textColor;
+
+        appendTextByWords(text);
+
+        this.currColor = previousColor;
+        return this;
+    }
+
+    // --------------------------------------------------------------------------------
+
+    public SmartLabel withTextColor(Color color) {
+        this.currColor = color;
+        return this;
+    }
+
+    public SmartLabel withNewlines(int count) {
+        this.lastLine += count;
+        this.lastLeft = 0;
+
+        this.startOfLine = true;
+
+        return this;
+    }
+
+    // --------------------------------------------------------------------------------
+
+//    public enum TextJustification {
+//        LEFT, CENTER, RIGHT;
+//    }
 //
-//    @Override public float getContentWidth() { return 0; }
-//    @Override public float getContentHeight() { return 0; }
-//
-//    @Override
-//    protected void renderWidget(SpriteBatch sb) {
+//    public SmartLabel withJustification(TextJustification justification) {
 //
 //    }
-//}
+
+//    public SmartLabel withLeftJustify() {
+//
+//    }
+//
+//    public SmartLabel withCenterJustify() {
+//
+//    }
+//
+//    public SmartLabel withRightJustify() {
+//
+//    }
+
+    // --------------------------------------------------------------------------------
+
+    private void finalizeTextGroup() {
+        String contents = stringBuilder.toString();
+
+        if (!contents.isEmpty()) {
+            float width = FontHelper.getWidth(font, contents, 1.0f) / Settings.scale;
+
+            groups.add(new TextGroup(contents, lastLine, currColor, width));
+
+            // Recompute the total textWidth
+            if (lastLeft > textWidth)
+                textWidth = lastLeft;
+
+            // Reset
+            stringBuilder = new StringBuilder();
+        }
+    }
+
+    private void appendTextByWords(String text) {
+        for (String word : text.split(" ")) {
+            float wordWidth = FontHelper.getWidth(font, word, 1.0f);
+
+            // Make a new line
+            if (lastLeft + spaceWidth + wordWidth > lineWidth) {
+                finalizeTextGroup();
+
+                // Start the next text group on a new line
+                stringBuilder.append(word);
+                lastLeft = wordWidth;
+                lastLine = lastLine + 1;
+            }
+            // Fits on current line
+            else {
+                if (startOfLine)
+                    startOfLine = false;
+                else
+                    stringBuilder.append(" ");
+
+                stringBuilder.append(word);
+
+                lastLeft += (wordWidth + spaceWidth);
+            }
+        }
+
+        finalizeTextGroup();
+        recomputeTextHeight();
+    }
+
+    private void recomputeTextHeight() {
+        if (groups.isEmpty())
+            textHeight = 0;
+        else {
+            int numSpacing = groups.get(groups.size() - 1).line;
+            textHeight = (numSpacing * lineSpacing) + ((numSpacing + 1) * fontLineHeight);
+        }
+    }
+
+    // --------------------------------------------------------------------------------
+
+    @Override public float getContentWidth() { return textWidth; }
+    @Override public float getContentHeight() { return textHeight; }
+
+    @Override
+    protected void renderWidget(SpriteBatch sb) {
+        int currLine = 0;
+
+        float contentLeft = getContentLeft();
+
+        float left = contentLeft;
+        float top = getContentTop();
+
+        for (TextGroup group : groups) {
+            String text = group.text;
+            Color color = group.color;
+
+            while (currLine < group.line) {
+                top -= (fontLineHeight + lineSpacing);
+                ++currLine;
+
+                left = contentLeft;
+            }
+
+            FontHelper.renderFontLeftTopAligned(sb, font, text, left * Settings.xScale, top * Settings.yScale, color);
+
+            left += group.width;
+        }
+
+//        EaselGraphicsHelper.drawDebugRects(sb, this);
+    }
+}
